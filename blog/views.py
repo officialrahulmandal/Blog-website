9af803b8174f56b587e6d86051f65658eb4a21f2
Rahul kumar <officialrahulmandal@gmail.com>
2018-02-20 20:47:36 +0530

    details page added

--- blog/views.py
+++ blog/views.py
@@ -10,5 +10,5 @@ def post_list(request):
 	posts=blog.published.all()
 	return render(request,'blog/post/list.html',{'posts':posts})
 def post_detail(request,year,month,day,post):
-	post=get_object_or_404(blog,slug=post,status='published',publish_year=year,publish_month=month,publish_day=day)
-	return render(request,'blog/post/details.html',{'post':post})
+	post=get_object_or_404(blog,slug=post,status='published',publish__year=year,publish__month=month,publish__day=day)
+	return render(request,'blog/post/detail.html',{'post':post})
