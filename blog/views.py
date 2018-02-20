5e85671e201e77d047c1bfa042245d9612215bc0
Rahul kumar <officialrahulmandal@gmail.com>
2018-02-20 20:28:51 +0530

    blog app homepage done

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import blog
+
 
 # Create your views here.
+def post_list(request):
+	posts=blog.published.all()
+	return render(request,'blog/post/list.html',{'posts':posts})
+def post_detail(request,year,month,day,post):
+	post=get_object_or_404(blog,slug=post,status='published',publish_year=year,publish_month=month,publish_day=day)
+	return render(request,'blog/post/details.html',{'post':post})
