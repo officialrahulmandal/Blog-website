0eae4695209d56b0cf93e9008325b8b6c815d772
Rahul kumar <officialrahulmandal@gmail.com>
2018-02-22 16:44:20 +0530

    saving..

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .models import blog
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 class PostListView(ListView):
@@ -33,13 +34,22 @@ def post_detail(request,year,month,day,post):
 def post_share(request,post_id):
 	# Retrieve post by id
 	post = get_object_or_404(blog,id=post_id,status='published')
+	sent=False
+
 	if request.method == 'POST':
 		# form was submitted
 	    form = EmailPostForm(request.POST)
 	    if form.is_valid():
-	    	# Form fields passed validation
-	    	cd = form.cleaned_data
-	    	#...send email
-	    else:
+			cd = form.cleaned_data
+			post_url = request.build_absolute_uri(post.get_absolute_url())
+			subject='{}({}) recommends you reading "{}"'
+format(cd['name'],cd['email'],post.title)
+	        message ='Read "{}" at {} \n \n {}\'s comments:{}'.
+format(post.title,post_url,cd['name'],cd['comments'])
+	        send_mail(subject,message,'admin@myblog.com',
+[cd['to']])
+					   sent = True
+
+	 else:
 	    	form = EmailPostForm()
 	    return render(request,'blog/post/share.html',{'post':post,'form':form})
